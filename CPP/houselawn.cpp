806c1e1b17ffc34ac6c642f7f9909276034a8bbf
Sparks <43890328+csparks78@users.noreply.github.com>
2020-05-25 17:25:20 -0600

    Added code

---
+++ CPP/houselawn.cpp
@@ -0,0 +1,14 @@
+// kattis houselawn
+// Cliff Sparks
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    long long l;
+    int m;
+
+    return 0;
+}
---
+++ PY/transitwoes.py
@@ -0,0 +1,5 @@
+x = int(input())
+n = int(input())
+for i in range(n):
+    y = x - int(input()) + x
+print(y+x)
