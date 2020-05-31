262a17449f2fdcdb463f860ebacfbb90aeb09c8b
Sparks <43890328+csparks78@users.noreply.github.com>
2020-05-31 14:16:34 -0600

    added code

---
+++ CPP/grassseed.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << fixed << setprecision(7);
+    float cost, l, w; 
+    int lawns;
+    float total, temp;
+    int i;
+    cin >> cost >> lawns;
+
+    for (i = 0; i < lawns; i++)
+    {
+        cin >> l >> w;
+        temp = l * w;
+        total += temp; 
+    }
+    total *= cost;
+    cout << total;
+
+    return 0;
+}
