d62fa7a8f872ef23ea23a9d083ec21c90a49c109
Sparks <43890328+csparks78@users.noreply.github.com>
2020-06-29 12:38:00 -0600

    Changes

---
+++ CPP/nodup.cpp
@@ -0,0 +1,19 @@
+// Cliff Sparks
+// Kattis nodup
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+   vector<string> animal_vec;
+   for(int i = 0; i<animal_vec.size(); i++) {
+      cin >> animal_vec[i];
+   }
+   // animal_vec.push_back("Elephant");
+   // animal_vec.push_back("Lion");
+   // animal_vec.push_back("Deer");
+   // animal_vec.push_back("Tiger");
+   for(int i = 0; i<animal_vec.size(); i++) {
+      cout << animal_vec[i] << endl;
+   }
+}
