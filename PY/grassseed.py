f7cbc6325ebeb440c56fa1415c4a26bfc792db5d
Sparks <43890328+csparks78@users.noreply.github.com>
2020-09-17 12:27:52 -0600

    updated

--- PY/grassseed.py
+++ PY/grassseed.py
@@ -14,32 +14,3 @@ while counter < lawns:
 total *= cost
 print total   
 
-# // Kattis - grassseed
-# // Cliff Sparks
-
-# #include <iostream>
-# #include <cmath>
-# #include <iomanip>
-
-# using namespace std;
-
-# int main()
-# {
-#     cout << fixed << setprecision(7);
-#     float cost, l, w; 
-#     int lawns;
-#     float total, temp;
-#     int i;
-#     cin >> cost >> lawns;
-
-#     for (i = 0; i < lawns; i++)
-#     {
-#         cin >> l >> w;
-#         temp = l * w;
-#         total += temp; 
-#     }
-#     total *= cost;
-#     cout << total;
-
-#     return 0;
-# }
