2d96c1080bcd7f1d993f37706cd62b14ac5781d7
Sparks <43890328+csparks78@users.noreply.github.com>
2020-11-15 14:12:15 -0700

    Added test cases

--- PY/simpleaddition.py
+++ PY/simpleaddition.py
@@ -2,10 +2,16 @@
 # Kattis 
 # simpleaddition
 
+def test():
+    assert add(3, 3) ==  6
+    assert add(100, 100) == 200
+    assert add(100000, 200000) == 300000
+    print("All test cases passed!")
+
 def add(a, b):
-    print(a+b)
-    
+    return(a+b)
 
 a = int(input())
 b = int(input())
+test()
 add(a,b)
