c0c971ff00140c765d71e79bfeaee3502aa3772e
Sparks <43890328+csparks78@users.noreply.github.com>
2021-10-18 10:59:09 -0600

    playing

--- PY/sandbox.py
+++ PY/sandbox.py
@@ -1,16 +1,9 @@
 
+y = {}
+int(y)
+x = int()
+for i in range(0,5):
+    y += i
+    print(y)
 
-import timeit, sys
-# x = 47
-# code = '''
-# global x
-# x = x + x
-# '''
-code = '''
-n = 100000 # 100K takes 0.056 seconds on my 2019 macbook pro
-ans = ''
-for i in range(n):
-    ans += str(i**2)
-print(ans)
-'''
-timeit.timeit(stmt=code, number=1)
+print(y)
---
+++ PY/shatteredCake.py
@@ -0,0 +1,4 @@
+num = input().split()
+x = int(num[0])
+for i in range(4):
+    print(num[i])
